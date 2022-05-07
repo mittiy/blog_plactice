d54bd4bad2ddb1e6c9410ee4cb0c62d568ae432d
mittiy <rouezura@yahoo.co.jp>
2022-05-07 21:54:23 +0900

    test

---
+++ src/libs/client.ts
@@ -0,0 +1,7 @@
+import { createClient } from "microcms-js-sdk"; //ES6
+
+// Initialize Client SDK.
+export const client = createClient({
+  serviceDomain: "5vlf8v6zvm", // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
+  apiKey: "824770bdea4e40dc8cf20f884696efe07c53",
+});
